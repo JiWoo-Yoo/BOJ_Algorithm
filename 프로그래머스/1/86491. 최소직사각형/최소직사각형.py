df2ca1fda4ca880c3c80b4cd75f3ae0cc04a56c1
JiWoo-Yoo <145994347+JiWoo-Yoo@users.noreply.github.com>
2024-04-03 23:22:09 +0900

    [level 1] Title: 최소직사각형, Time: 5.75 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -6,7 +6,7 @@ def solution(sizes):
         max_size = max(max(size), max_size)
         
     max_min = 0
-    # 작은 값을 찾아서
+    # 작은 값들 중 가장 큰 값을 찾아서
     for size in sizes:
         max_min = max(min(size), max_min)
     return max_size * max_min
