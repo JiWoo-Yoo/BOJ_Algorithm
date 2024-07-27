70fb4f08b7b27d43f75ee90a1ca19953ed88e523
Yoo Jiwoo <145994347+JiWoo-Yoo@users.noreply.github.com>
2024-07-27 11:47:43 +0900

    [Silver V] Title: 미아 노트, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/20114. 미아 노트/미아 노트.py
+++ 백준/Silver/20114. 미아 노트/미아 노트.py
@@ -6,6 +6,7 @@ for _ in range(h):
 
 answer = ''
 
+# 주어진 index에서 가로로 w, 세로로 h만큼 탐색하면서 '?'가 아닌 문자가 있으면 반환
 def find_c(x, h, w, strings):
     for i in range(h):
         for j in range(x, x + w):
@@ -13,6 +14,7 @@ def find_c(x, h, w, strings):
                 return strings[i][j]
     return '?'
 
+# x축으로 가로(w)로 번진만큼 건너뛰며 find_c 실행
 for x in range(0, len(strings[0]), w):
     answer += find_c(x, h, w, strings)
 
